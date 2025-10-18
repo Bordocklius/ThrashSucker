4546cc63eed49d21d5add3b5d88b3d77530d10cb
Bordocklius <miguel.dhaenens@hotmail.com>
2025-10-18 15:39:10 +0200

    Added cannon suck and aiming

--- Assets/Scripts/Presenters/SuckingCannon.cs
+++ Assets/Scripts/Presenters/SuckingCannon.cs
@@ -8,6 +8,7 @@ namespace ThrashSucker.Presenters
     public class SuckingCannon : MonoBehaviour
     {
         public List<GameObject> AmmoList = new List<GameObject>();
+        public int MaxAmmoCount;
 
         [SerializeField]
         private Transform _barrelPoint;
@@ -17,6 +18,13 @@ namespace ThrashSucker.Presenters
         [SerializeField]
         private float _maxSuctionForce;
         private float _suctionForce;
+
+        [SerializeField]
+        private float _range;
+        [SerializeField]
+        private Camera _mainCamera;
+        [SerializeField]
+        private Transform _crosshairTransform;
 
         [SerializeField]
         private LayerMask _layerMask;
@@ -65,6 +73,55 @@ namespace ThrashSucker.Presenters
             IsCannonSucking = !IsCannonSucking;
             if (!IsCannonSucking)
                 _suctionForce = 0f;
+        }
+
+        private void OnCannonShoot(InputValue inputValue)
+        {
+            GetAimDirection();
+            //GameObject obj = AmmoList[AmmoList.Count - 1].gameObject;
+            //Rigidbody rb = obj.GetComponent<Rigidbody>();
+            //if (rb != null)
+            //{
+            //    Vector3 direction = GetAimDirection();
+            //    BallpitBall ball = obj.GetComponent<BallpitBall>();
+            //    if (ball != null)
+            //    {
+            //        ball.IsShot = true;
+            //        ball.Rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            //    }
+            //    obj.transform.position = _barrelPoint.position;
+            //    obj.SetActive(true);
+            //    rb.AddForce(direction * _shootForce, ForceMode.Impulse);
+            //    AmmoList.Remove(obj);
+            //}
+            //Text.text = AmmoList.Count.ToString();
+
+        }
+
+        private Vector3 GetAimDirection()
+        {
+            // Shoot ray to crosshair
+            Ray ray = _mainCamera.ScreenPointToRay(_crosshairTransform.position);
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction * _range, Color.red, 2f, true);
+            Physics.Raycast(ray, out hit, _range);
+
+            if (hit.collider == null)
+            {
+                hit.point = ray.origin + ray.direction * _range;
+            }
+
+
+            Debug.DrawLine(ray.origin, hit.point, Color.blue, 2f, true);
+
+            // Get direction from shootpoint to hitpoint
+            Vector3 direction = (hit.point - _barrelPoint.position).normalized;
+            //float randomStrength = Random.Range(0, _offsetStrengthMax);
+            //direction = (direction + Random.insideUnitSphere * randomStrength).normalized;
+            Debug.DrawLine(_barrelPoint.position, direction * _range, Color.green, 2f);
+
+            return direction;
+
         }
 
         private void OnDrawGizmos()
---
+++ Assets/Scripts/Presenters/SuckingCannonCollection.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThrashSucker.Presenters
+{
+    public class SuckingTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private LayerMask _layerMask;
+
+        [SerializeField]
+        private SuckingCannon _canon;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            GameObject obj = other.gameObject;
+            if (obj != null && _layerMask == (_layerMask | (1 << obj.layer)) && _canon.IsCannonSucking)
+            {
+                if (_canon.AmmoList.Count < _canon.MaxAmmoCount)
+                {
+                    _canon.AmmoList.Add(obj);
+                    obj.SetActive(false);
+                }
+            }
+        }
+    }
+}
