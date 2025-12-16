f9c33f4f4d069a3989a41c8e732907c52f779311
Bordocklius <miguel.dhaenens@hotmail.com>
2025-12-16 09:12:06 +0100

    Added healthbar & presenter script

--- Assets/Scripts/Presenters/PlayerHealthPresenter.cs
+++ Assets/Scripts/Presenters/PlayerHealthPresenter.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TrashSucker.Presenters
 {
-    public class PlayerHealthPresenter
+    public class PlayerHealthPresenter: MonoBehaviour
     {
+        [SerializeField]
+        private PlayerMovementPresenter _playerPresenter;
+
+        [SerializeField]
+        private Image _healthBar;
+
+        private float _startingHP;
+
+        private void Start()
+        {
+            _startingHP = _playerPresenter.StartingHP;
+        }
+
+        private void Update()
+        {
+            _healthBar.fillAmount = _playerPresenter.Health / _playerPresenter.StartingHP;
+        }
     }
 }
